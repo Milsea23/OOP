c2b23b0141dfc7c43effe6ed8ea1184327b3fa9a
Sean M <Sean M@192.168.2.40>
2023-09-12 23:21:29 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[]args) {
+		Pen p = new Pen();
+		Headphones h = new Headphones();
+		
+		System.out.println(p.colour);
+		System.out.println(p.point);
+		System.out.println(p.type);		
+		System.out.println(p.clicked);
+		
+		System.out.println();
+
+		System.out.println(h.charge);
+		System.out.println(h.colour);
+		System.out.println(h.power);
+		System.out.println(h.volume);
+
+
+	}
+}
